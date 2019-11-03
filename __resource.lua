ec0e7064dc5572e8e320363d03558047f5ad46a8
KingRyuShin <42899923+KingRyuShin@users.noreply.github.com>
2019-11-03 12:21:10 -0500

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'PXRP VIP Status'
+
+version '1.0.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
+
+dependencies {
+	'es_extended'
+}
